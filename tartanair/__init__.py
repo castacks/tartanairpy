41e18d871dfaef0619b615145f75aa1bbb978fb6
yoraish <yorai.shaoul@gmail.com>
2023-02-21 19:38:47 -0500

    feat(ta): major interface refactor.

---
+++ tartanair/__init__.py
@@ -0,0 +1,2 @@
+from . import tartanair
+from .tartanair import *
---
+++ tartanair/tartanair.py
@@ -0,0 +1,76 @@
+# General imports.
+import os
+import numpy as np
+
+# Local imports.
+from .downloader import TartanAirDownloader
+from .dataset import TartanAirDataset
+from .customizer import TartanAirCustomizer
+
+# TODO(yoraish):
+'''
+[ ] Auto install azcopy.
+[ ] Check that the inputs are valid.
+[ ] Add a function to customize a trajectory.
+[ ] Verify download of flow.
+[ ] Remove spamming text.
+'''
+print("TartanAir toolbox initialized.")
+
+tartanair_data_root = ""
+downloader = None
+dataset = None
+customizer = None
+
+def init(tartanair_data_root_input):
+    """
+    Initialize the TartanAir toolbox.
+    """
+    global tartanair_data_root
+    tartanair_data_root = tartanair_data_root_input
+
+    global downloader
+    downloader = TartanAirDownloader(tartanair_data_root)
+
+    global dataset
+    dataset = TartanAirDataset(tartanair_data_root)
+
+    global customizer
+    customizer = TartanAirCustomizer(tartanair_data_root)
+    
+
+def download(env = [], difficulty = [], trajectory_id = [], modality = 'image', camera_name = 'lcam_front'):
+    """
+    Download the relevant data from the TartanAir dataset.
+    """
+    global downloader
+    global tartanair_data_root
+
+    if not tartanair_data_root:
+        raise Exception("TartanAir toolbox not initialized. Please call tartanair.init(tartanair_data_root) first.")
+    else:
+        downloader.download(env, difficulty, trajectory_id, modality, camera_name)
+
+def customize(env, difficulty, trajectory_id, modality = 'image', new_camera_models_params = [{}], num_workers = 1):
+    """"
+    Synthesizes data in new camera-models from the TartanAir dataset.
+    """
+    global customizer
+    global tartanair_data_root
+
+    if not tartanair_data_root:
+        raise Exception("TartanAir toolbox not initialized. Please call tartanair.init(tartanair_data_root) first.")
+    customizer.customize(env, difficulty, trajectory_id, modality, new_camera_models_params, num_workers=num_workers)
+
+def create_image_dataset(env, difficulty, trajectory_id, modality = 'image', camera_name = 'lcam_front', transform = None):
+    """
+    Return the relevant data from the TartanAir dataset.
+    This dataset will only handle image data in modalities such as 'image', depth, and segmentation.
+    """
+    global dataset
+    global tartanair_data_root
+    
+    if not tartanair_data_root:
+        raise Exception("TartanAir toolbox not initialized. Please call tartanair.init(tartanair_data_root) first.")
+
+    return dataset.create_image_dataset(env, difficulty, trajectory_id, modality, camera_name, transform)
