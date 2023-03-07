c8d5218e16514479629edba3abe542810d35eb6c
yoraish <yorai.shaoul@gmail.com>
2023-03-07 22:02:49 +0200

    trying docs

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-autodoc_mock_imports = ["cupy"]
+autodoc_mock_imports = ["cupy", "numba"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
