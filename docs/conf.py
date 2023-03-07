728c23ec1b3fa6c93c963385cb553565f31a0471
yoraish <yorai.shaoul@gmail.com>
2023-03-07 17:27:17 +0200

    try(docs): faking imports.

--- docs/conf.py
+++ docs/conf.py
@@ -26,8 +26,7 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
+autodoc_mock_imports = [ "TartanAirDownloader", "TartanAirDataset", "TartanAirCustomizer", "TartanAirLister", "TartanAirVisualizer", "TartanAirIterator", "TartanAirEvaluator", "TartanAirTrajectoryReader"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
