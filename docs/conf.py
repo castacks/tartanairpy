1dbc1faf9dd9205b1e1ef04af65d77b2f19c7501
Amigoshan <wenshanw@andrew.cmu.edu>
2025-06-02 12:08:34 -0400

    doc dependency

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.append(os.path.abspath('..'))
 
 project = 'TartanAir'
 copyright = 'Carnegie Mellon University, 2023, Wenshan Wang, Yaoyu Hu, Yuheng Qiu, Shihao Shen, Yorai Shaoul.'
-author =  'Wenshan Wang, Yaoyu Hu, Yuheng Qiu, Shihao Shen, Yorai Shaoul.'
+author =  'Wenshan Wang, Yaoyu Hu, Yuheng Qiu, Shihao Shen, Yorai Shaoul, Yuchen Zhang, Srivatsa Grama Satyanarayana.'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -28,7 +28,7 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-autodoc_mock_imports = ["colorama", "cupy", "cupy_cuda117", "kornia", "matplotlib", "networkx", "numba", "numpy", "cv2", "Pillow", "plyfile", "pyquaternion", "pytransform3d", "PyYAML", "yaml", "scipy", "torch", "torchvision", "tqdm", "SixPlanarNumba", ".downloader", ".dataset", ".customizer", ".lister", ".visualizer", ".iterator", ".evaluator", ".reader", "TartanAirDownloader", "TartanAirDataset", "TartanAirCustomizer", "TartanAirLister", "TartanAirVisualizer", "TartanAirIterator", "TartanAirEvaluator", "TartanAirTrajectoryReader", 'tartanair.image_resampling.image_sampler', 'tartanair.image_resampling.mvs_utils.camera_models', 'tartanair.image_resampling.mvs_utils.shape_struct', 'tartanair.image_resampling.mvs_utils.blend_function', 'wget', 'TartanAirDataLoader', 'MultiDatasets', 'data_cacher', 'pandas', 'tartanair.data_cacher.MultiDatasets', '.data_cacher.MultiDatasets', 'data_cacher.MultiDatasets', '.MultiDatasets']
+autodoc_mock_imports = ["sphinxcontrib-youtube", "colorama", "cupy", "cupy_cuda117", "kornia", "matplotlib", "networkx", "numba", "numpy", "cv2", "Pillow", "plyfile", "pyquaternion", "pytransform3d", "PyYAML", "yaml", "scipy", "torch", "torchvision", "tqdm", "SixPlanarNumba", ".downloader", ".dataset", ".customizer", ".lister", ".visualizer", ".iterator", ".evaluator", ".reader", "TartanAirDownloader", "TartanAirDataset", "TartanAirCustomizer", "TartanAirLister", "TartanAirVisualizer", "TartanAirIterator", "TartanAirEvaluator", "TartanAirTrajectoryReader", 'tartanair.image_resampling.image_sampler', 'tartanair.image_resampling.mvs_utils.camera_models', 'tartanair.image_resampling.mvs_utils.shape_struct', 'tartanair.image_resampling.mvs_utils.blend_function', 'wget', 'TartanAirDataLoader', 'MultiDatasets', 'data_cacher', 'pandas', 'tartanair.data_cacher.MultiDatasets', '.data_cacher.MultiDatasets', 'data_cacher.MultiDatasets', '.MultiDatasets']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
